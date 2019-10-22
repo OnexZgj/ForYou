1fd20beca44eae178715a1be692e3f7afd99106f
OnexZgj <474835552@qq.com>
2019-10-22 17:56:22 +0800

    Update TodoSection.java

--- app/src/main/java/com/it/onex/foryou/bean/TodoSection.java
+++ app/src/main/java/com/it/onex/foryou/bean/TodoSection.java
@@ -2,10 +2,7 @@ package com.it.onex.foryou.bean;
 
 import com.chad.library.adapter.base.entity.SectionEntity;
 
-/**
- * 作者：wangwnejie on 2018/8/9 14:33
- * 邮箱：wang20080990@163.com
- */
+
 public class TodoSection extends SectionEntity<TodoTaskDetail.DatasBean> {
 
     public TodoSection(boolean isHeader, String header) {
